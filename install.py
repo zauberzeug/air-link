9a19e61341517bcc9ad6b7799c32d8da699e1239
Rodja Trappe <rodja@zauberzeug.com>
2023-11-22 08:58:43 +0100

    starting air_admin as service

--- install.py
+++ install.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import re
+import tempfile
 from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
 
 import run
 
@@ -16,11 +19,13 @@ if nv_tegra_release_path.is_file():
         l4t_version = f"{l4t_release}.{l4t_revision}"
         if l4t_release == "32":
             if not run.sh('python3.8 --version'):
-                print('installing Python 3.8')
-                run.sh('sudo apt install software-properties-common -y')
-                run.sh('sudo add-apt-repository ppa:deadsnakes/ppa -y')
-                run.sh('sudo apt install python3.8 -y')
-                run.sh('curl https://bootstrap.pypa.io/get-pip.py | python3.8')
+                print('installing Python 3.8', flush=True)
+                run.sh('sudo apt install software-properties-common -y',
+                       'sudo add-apt-repository ppa:deadsnakes/ppa -y',
+                       'sudo apt install python3.8 -y',
+                       'curl https://bootstrap.pypa.io/get-pip.py | python3.8')
+            else:
+                print('Python 3.8 already installed',flush=True)
             run.python_cmd = 'python3.8'
 
 run.pip('install -r requirements.txt')
@@ -34,3 +39,14 @@ with authorized_keys_file.open('a') as file:
         key = key_file.read_text().strip()
         if key and key not in existing_keys:
             file.write(f'{key}\n')
+
+template = Environment(loader=FileSystemLoader('.')).get_template('air_admin.service.j2')
+with tempfile.NamedTemporaryFile(mode='w+') as temp_file:
+    temp_file.write(template.render(python_cmd=run.python_cmd))
+    temp_file.flush()
+    run.sh(f'sudo cp {temp_file.name} /etc/systemd/system/air_admin.service')
+    run.sh(
+        'sudo systemctl daemon-reload',
+        'sudo systemctl enable air_admin.service',
+        'sudo systemctl restart air_admin.service',
+    )
--- run.py
+++ run.py
@@ -1,12 +1,27 @@
+import logging
+import os
 import subprocess
 
 python_cmd = 'python3'
+sudo_password = ''
 
-def sh(cmd:str) -> bool:
-    try:
-        subprocess.run(cmd, shell=True, check=True)
-    except subprocess.CalledProcessError:
-        return False
+def sh(*cmds) -> bool:
+    env = os.environ.copy()
+    for cmd in cmds:
+        if cmd.startswith('sudo '):
+            if not sudo_password:
+                logging.error('sudo password not set')
+                return False
+            env['SUDO_PASSWORD'] = sudo_password
+            cmd = cmd.replace('sudo ', 'echo $SUDO_PASSWORD | sudo -S ', 1)
+        try:
+            subprocess.run(cmd, shell=True, check=True, env=env)
+        except subprocess.CalledProcessError as e:
+            logging.error(f'failed to run {cmd}: {e.output}')
+            return False
+        except Exception:
+            logging.exception(f'failed to run {cmd}')
+            return False   
     return True
 
 def pip(cmd:str) -> bool:
