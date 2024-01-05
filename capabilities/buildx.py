6a7a5b66d28aa3acd76f2dedda1f795e595956bc
Rodja Trappe <rodja@zauberzeug.com>
2024-01-05 05:31:29 +0100

    newer buildx

--- capabilities/buildx.py
+++ capabilities/buildx.py
@@ -11,7 +11,7 @@ class Buildx(Capability):
     def install(self):
         run.sh(
             'mkdir -p ~/.docker/cli-plugins/',
-            'wget -O ~/.docker/cli-plugins/docker-buildx https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.linux-arm64',
+            'wget -O ~/.docker/cli-plugins/docker-buildx https://github.com/docker/buildx/releases/download/v0.12.0/buildx-v0.12.0.linux-arm64',
             'chmod a+x ~/.docker/cli-plugins/docker-buildx',
         )
 
