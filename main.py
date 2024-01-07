223b171c9f24d92236b0e0d2310d992a65593e83
Rodja Trappe <rodja@zauberzeug.com>
2024-01-07 13:37:16 +0100

    fix calling instance

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import os
 from typing import Any, Dict
 
 import icecream
+import nicegui.air
 from dotenv import load_dotenv
 from nicegui import app, background_tasks, core, ui
-import nicegui.air
 
 logging.basicConfig(level=logging.INFO)
 icecream.install()
@@ -26,14 +26,14 @@ ui.label('Air Admin').classes('text-4xl')
 async def startup():
     incoming: Dict[str, asyncio.StreamWriter] = {}
 
-    @core.air.instance.relay.on('ssh_data')
+    @core.air.relay.on('ssh_data')
     def from_socketio_to_tcp(data: Dict[str, Any]) -> None:
         if data['ssh_id'] in incoming:
             incoming[data['ssh_id']].write(data['payload'])
         else:
             logging.warning(f'received data for unknown ssh_id {data["ssh_id"]}')
 
-    @core.air.instance.relay.on('connect_ssh')
+    @core.air.relay.on('connect_ssh')
     async def connect_ssh(data: Dict[str, str]) -> None:
         ssh_id = data['ssh_id']
         try:
@@ -49,7 +49,7 @@ async def startup():
             while not reader.at_eof():
                 payload = await reader.read(1024)
                 if payload:
-                    await core.air.instance.relay.emit('ssh_data', {'ssh_id': ssh_id, 'payload': payload})
+                    await core.air.relay.emit('ssh_data', {'ssh_id': ssh_id, 'payload': payload})
         except ConnectionResetError:
             logging.exception(f'Connection reset by peer for ssh_id {ssh_id}, payload: {payload.decode() if payload else "empty"}')
         except Exception:
@@ -58,7 +58,7 @@ async def startup():
             logging.info(f'ssh connection for {ssh_id} at eof or error')
             await disconnect_ssh({'ssh_id': ssh_id})
 
-    @core.air.instance.relay.on('disconnect_ssh')
+    @core.air.relay.on('disconnect_ssh')
     async def disconnect_ssh(data: Dict[str, str]) -> None:
         writer = incoming.pop(data['ssh_id'])
         await writer.drain()
