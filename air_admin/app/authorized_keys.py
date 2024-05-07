908f81b717fa6602c45b223cb804cc8201cecac6
Falko Schindler <falko@zauberzeug.com>
2024-05-07 17:07:02 +0200

    allow managing authorized keys

---
+++ air_admin/app/authorized_keys.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+from nicegui import ui
+
+
+class AuthorizedKeysDialog(ui.dialog):
+
+    def open(self) -> None:
+        super().open()
+        path = Path('~/.ssh/authorized_keys').expanduser()
+        columns = [
+            {'name': 'name', 'label': 'Name', 'field': 'name', 'align': 'left'},
+            {'name': 'type', 'label': 'Type', 'field': 'type', 'align': 'left'},
+            {'name': 'key', 'label': 'Key', 'field': 'key', 'align': 'left'},
+        ]
+        rows = [
+            {'name': line.split()[2], 'type': line.split()[0], 'key': line.split()[1]}
+            for line in path.read_text().splitlines()
+        ]
+        self.clear()
+        with self.props('full-width'), ui.card():
+            ui.label('Authorized keys').classes('text-xl')
+            table = ui.table(columns=columns, rows=rows, row_key='name', selection='single').classes('w-full')
+            with table.add_slot('bottom'), ui.row().classes('items-center mt-4 w-full'):
+                def save_keys():
+                    path.write_text(''.join(f'{row["type"]} {row["key"]} {row["name"]}\n' for row in table.rows))
+
+                def add_key():
+                    words = key.value.split()
+                    assert len(words) == 3
+                    table.add_rows({'name': words[2], 'type': words[0], 'key': words[1]})
+                    key.value = None
+                    save_keys()
+
+                def remove_key():
+                    table.remove_rows(*table.selected)
+                    save_keys()
+
+                key = ui.input('New key').props('outlined').on('keydown.enter', add_key).classes('flex-grow')
+                ui.button('Add', on_click=add_key).bind_enabled_from(key, 'value')
+                ui.button('Remove', on_click=remove_key).bind_enabled_from(table, 'selected', bool)
--- air_admin/app/main_page.py
+++ air_admin/app/main_page.py
@@ -1,10 +1,26 @@
 from nicegui import app, ui
 
+from .authorized_keys import AuthorizedKeysDialog
+
 
 def create_page() -> None:
     @ui.page('/')
     def main_page():
-        ui.label('Air Admin').classes('text-4xl')
-        ui.button(on_click=app.shutdown, icon='power_settings_new')
+        with ui.header().classes('items-center'):
+            ui.label('Air Admin').classes('text-4xl')
 
-        ui.input('Air Admin Token').bind_value(app.storage.general, 'air_admin_token')
+            ui.space()
+
+            authorized_keys = AuthorizedKeysDialog()
+            ui.button(icon='key', on_click=authorized_keys.open) \
+                .tooltip('Manage authorized keys') \
+                .props('flat round color=white')
+
+            ui.separator().props('vertical')
+
+            ui.input('Air Admin Token', password=True, password_toggle_button=True) \
+                .bind_value(app.storage.general, 'air_admin_token') \
+                .props('dark')
+            ui.button(on_click=app.shutdown, icon='power_settings_new') \
+                .tooltip('Restart Air Admin') \
+                .props('flat round color=white')
