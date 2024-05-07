0895332714b5a5908b20ebac316283b896824426
Paula Kammler <paula@zauberzeug.com>
2024-05-07 16:08:52 +0200

    add input for air admin token and store it in app.storage.general

--- air_admin/app/main_page.py
+++ air_admin/app/main_page.py
@@ -1,7 +1,10 @@
-from nicegui import ui
+from nicegui import app, ui
 
 
 def create_page() -> None:
     @ui.page('/')
     def main_page():
         ui.label('Air Admin').classes('text-4xl')
+        ui.button(on_click=app.shutdown, icon='power_settings_new')
+
+        ui.input('Air Admin Token').bind_value(app.storage.general, 'air_admin_token')
--- air_admin/main.py
+++ air_admin/main.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 import logging
-import os
 
-import nicegui.air
 from app import create_main_page, setup_ssh
-from dotenv import load_dotenv
 from nicegui import app, ui
 
 logging.basicConfig(level=logging.INFO)
-load_dotenv('.env')
 
-if 'ON_AIR_SERVER' in os.environ:
-    nicegui.air.RELAY_HOST = os.environ['ON_AIR_SERVER']
+if app.storage.general.get('air_admin_token'):
     app.on_startup(setup_ssh)
 
 create_main_page()
 
-ui.run(favicon='⛑', reload=False)
+ui.run(favicon='⛑', reload=False, on_air=app.storage.general.get('air_admin_token', False))
