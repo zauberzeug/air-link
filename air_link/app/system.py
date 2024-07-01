119256473d193519eb68b574bdb3572216091ffe
Falko Schindler <falko@zauberzeug.com>
2024-07-01 14:35:35 +0200

    code review

--- air_link/app/system.py
+++ air_link/app/system.py
@@ -3,15 +3,10 @@ import shutil
 from nicegui import ui
 
 
-def get_disk_space() -> tuple[float, float]:
-    total, _, free = shutil.disk_usage('/')
-    return total / 2**30, free / 2**30
-
-
 @ui.refreshable
 def show_disk_space() -> ui.label:
-    total, free = get_disk_space()
-    label = ui.label(f'Free disk space: {free:.1f} GB / {total:.1f} GB')
+    total, _, free = shutil.disk_usage('/')
+    label = ui.label(f'Free disk space: {free / 2**30:.1f} GB / {total / 2**30:.1f} GB')
     if free / total < 0.1:
-        return label.classes('text-negative').tooltip(f'Low disk space! {free / total * 100:.1f}% left')
+        return label.classes('text-negative').tooltip(f'Low disk space! {free / total:.1%} left')
     return label
