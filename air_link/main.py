c36e1089d7d37b9db4a367f1ca5b76ccad69a34c
Falko Schindler <falko@zauberzeug.com>
2024-07-05 10:17:13 +0200

    code review

--- air_link/main.py
+++ air_link/main.py
@@ -4,7 +4,7 @@ from .install import install
 from .run import run
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(
         'Air Link', description='SSH access, diagnostics and administration for edge devices.')
     parser.add_argument('action', choices=['install', 'run'], help='action to perform', default='run')
