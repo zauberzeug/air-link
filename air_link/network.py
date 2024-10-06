b7b2e9249d23f641c35ea0d9ff4a13e5bd2a91cd
Rodja Trappe <rodja@zauberzeug.com>
2024-10-06 09:17:09 +0200

    Ensure we only run one network check at a time

--- air_link/network.py
+++ air_link/network.py
@@ -1,26 +1,29 @@
+import asyncio
 import time
 
 import aioping
 from nicegui import app, ui
 
 HISTORY_SIZE = 300
+lock = asyncio.Lock()
 
 
 async def collect_data() -> None:
-    try:
-        latency = await aioping.ping('8.8.8.8', timeout=1)
-    except TimeoutError:
-        latency = None
-    state = 'down' if latency is None else 'bad' if latency > 0.3 else 'good'
-    events = app.storage.general.setdefault('network', [])
-    if events and state == events[-1][1]:
-        return
+    async with lock:
+        try:
+            latency = await aioping.ping('8.8.8.8', timeout=2)
+        except TimeoutError:
+            latency = None
+        state = 'down' if latency is None else 'bad' if latency > 1.0 else 'good'
+        events = app.storage.general.setdefault('network', [])
+        if events and state == events[-1][1]:
+            return
 
-    timestamp = time.strftime(r'%Y-%m-%d %H:%M:%S')
-    print('network:', state)
-    events.append((timestamp, state))
-    while len(events) > HISTORY_SIZE:
-        events.pop(0)
+        timestamp = time.strftime(r'%Y-%m-%d %H:%M:%S')
+        print('network:', state, flush=True)
+        events.append((timestamp, state))
+        while len(events) > HISTORY_SIZE:
+            events.pop(0)
 
 
 def setup() -> None:
