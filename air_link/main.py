555609d4edda580fe6f16921a5f38a2c9a6aac5f
Falko Schindler <falko@zauberzeug.com>
2025-04-01 09:22:13 +0200

    review

--- air_link/main.py
+++ air_link/main.py
@@ -7,11 +7,11 @@ from .run import run
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(
-        'Air Link', description='SSH access, diagnostics and administration for edge devices.')
+    parser = argparse.ArgumentParser('Air Link',
+                                     description='SSH access, diagnostics and administration for edge devices.')
     parser.add_argument('action', choices=['install', 'run', 'set-token'], help='action to perform', default='run')
     parser.add_argument('token', nargs='?',
-                        help='On Air token for remote access (used with install or set-token actions)')
+                        help='On Air token for remote access (used with "install" or "set-token" actions)')
     args = parser.parse_args()
 
     if args.action == 'run':
@@ -21,7 +21,9 @@ def main() -> None:
         if args.token:
             app.storage.general['air_link_token'] = args.token
         install()
-        print('Installation complete.' + 'Token is saved' if args.token else '')
+        print('Installation complete.')
+        if args.token:
+            print('Token is saved.')
 
     if args.action == 'set-token':
         if not args.token:
