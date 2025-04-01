3437b48ab90c3415bb469750817bd4b8d2beae5b
Rodja Trappe <rodja@zauberzeug.com>
2025-04-01 07:45:15 +0200

    allow to set token directly from terminal

--- air_link/main.py
+++ air_link/main.py
@@ -1,5 +1,7 @@
 import argparse
 
+from nicegui import app
+
 from .install import install
 from .run import run
 
@@ -7,11 +9,23 @@ from .run import run
 def main() -> None:
     parser = argparse.ArgumentParser(
         'Air Link', description='SSH access, diagnostics and administration for edge devices.')
-    parser.add_argument('action', choices=['install', 'run'], help='action to perform', default='run')
+    parser.add_argument('action', choices=['install', 'run', 'set-token'], help='action to perform', default='run')
+    parser.add_argument('token', nargs='?',
+                        help='On Air token for remote access (used with install or set-token actions)')
     args = parser.parse_args()
 
     if args.action == 'run':
         run()
 
     if args.action == 'install':
+        if args.token:
+            app.storage.general['air_link_token'] = args.token
         install()
+        print('Installation complete.' + 'Token is saved' if args.token else '')
+
+    if args.action == 'set-token':
+        if not args.token:
+            print('Error: token argument is required')
+            return
+        app.storage.general['air_link_token'] = args.token
+        print('Token is saved.')
