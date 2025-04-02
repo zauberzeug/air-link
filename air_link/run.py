c625a456d1ba99afe4de82404adfb576773a2b19
Rodja Trappe <rodja@zauberzeug.com>
2025-04-02 19:41:34 +0200

    make port configurable

--- air_link/run.py
+++ air_link/run.py
@@ -7,7 +7,7 @@ from .main_page import create_page
 from .package import read_env
 
 
-def run() -> None:
+def run(port=None) -> None:
     logging.basicConfig(level=logging.INFO)
     logging.getLogger('watchfiles').setLevel(logging.WARNING)
     logging.getLogger('nicegui.air').setLevel(logging.DEBUG)
@@ -20,4 +20,4 @@ def run() -> None:
 
     create_page()
     app.on_startup(read_env)
-    ui.run(title='Air Link', favicon='⛑', reload=False, on_air=on_air, show=False)
+    ui.run(title='Air Link', favicon='⛑', reload=False, on_air=on_air, show=False, port=port)
