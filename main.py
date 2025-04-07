92d285e52d956b3d6f6b434f4a84914b0804d03f
Rodja Trappe <rodja@zauberzeug.com>
2025-04-07 11:30:09 +0200

    Use .air-link storage path to not collide with other NiceGUI applications (#28)
    * change storage dir to .air-link
    
    * migrate from old storage

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
+import os
+import shutil
+from pathlib import Path
+
 from air_link import main
+
+os.environ['NICEGUI_STORAGE_PATH'] = '.air-link'
+
+old_storage_path = Path('.nicegui/general-storage.json')
+new_storage_path = Path('.air-link/general-storage.json')
+
+if old_storage_path.exists() and not new_storage_path.exists():
+    try:
+        new_storage_path.parent.mkdir(exist_ok=True)
+        shutil.copy(old_storage_path, new_storage_path)
+        print('Migrated storage from .nicegui to .air-link')
+    except Exception as e:
+        print(f'Error migrating storage: {e}')
+
 
 if __name__ == '__main__':
     main()
