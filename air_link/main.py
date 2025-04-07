10e5c9e06e98fa5c97b4b773b2b2b6b847b29caa
Rodja Trappe <rodja@zauberzeug.com>
2025-04-07 11:28:30 +0200

    Use the non-common port 4230 as default, but make it configurable (#27)
    * make port configurable
    
    * use port 4230 as default to not collide with other installed software (this is a breaking change)
    
    * update docs to reflect new default port
    
    * Update air_link/main.py
    
    * Update air_link/run.py
    
    ---------
    
    Co-authored-by: Falko Schindler <falko@zauberzeug.com>

--- air_link/main.py
+++ air_link/main.py
@@ -12,18 +12,24 @@ def main() -> None:
     parser.add_argument('action', choices=['install', 'run', 'set-token'], help='action to perform', default='run')
     parser.add_argument('token', nargs='?',
                         help='On Air token for remote access (used with "install" or "set-token" actions)')
+    parser.add_argument('--port', type=int, help='Port where the app should be launched (default: 4230)')
     args = parser.parse_args()
 
     if args.action == 'run':
-        run()
+        port = args.port or app.storage.general.get('air_link_port', 4230)
+        run(port=port)
 
     if args.action == 'install':
         if args.token:
             app.storage.general['air_link_token'] = args.token
+        if args.port:
+            app.storage.general['air_link_port'] = args.port
         install()
         print('Installation complete.')
         if args.token:
             print('Token is saved.')
+        if args.port:
+            print('Port is saved.')
 
     if args.action == 'set-token':
         if not args.token:
